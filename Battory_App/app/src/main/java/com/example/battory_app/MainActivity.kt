87248f98690dc508fb1500a8caa55fe0b36f13e4
HyoPrize <hyskuk@tukorea.ac.kr>
2022-05-20 20:20:55 +0900

    Add test code

--- Battory_App/app/src/main/java/com/example/battory_app/MainActivity.kt
+++ Battory_App/app/src/main/java/com/example/battory_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.battory_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val customButton: Button = findViewById(R.id.custom_button)
+        customButton.setOnClickListener { clickBtn() }
+    }
+
+    private fun clickBtn() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
     }
 }
